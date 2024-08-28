15ed11622398ad044dd134acdb3f7463adfbe096
SAIKAT SAMANTA <saikat.samanta@hy-vee.com>
2024-08-28 22:04:32 +0530

    game

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,58 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IconsList from '../components/Home/IconsList'
 import Card from '../components/Card'
 
-const cardData = [
-  {
-    title: 'Discover New Destinations',
-    image: 'https://picsum.photos/id/1005/500/300',
-    description: 'Discover hidden gems like lighthouses, waterfalls, and breathtaking vistas.',
-    price: '1500',
-    live: true
-  },
-  {
-    title: 'Book Your Stay',
-    image: 'https://picsum.photos/id/1006/500/300',
-    description: 'Book your perfect stay at our luxurious properties.',
-    price: '2500',
-  },
-  {
-    title: 'Host Your Space',
-    image: 'https://picsum.photos/id/1007/500/300',
-    description: 'Elevate your home to a world-class experience with our premium properties.',
-    price: '3500',
-    live:true
-  },
-  {
-    title: 'Join Our Community',
-    image: 'https://picsum.photos/id/1008/500/300',
-    description: 'Connect with like-minded hosts and share your experiences.',
-    price: '4500',
-  },
-  {
-    title: 'Discover Our Blog',
-    image: 'https://picsum.photos/id/1009/500/300',
-    description: 'Stay updated with the latest travel news and tips.',
-    price: '5500',
-  },
-  {
-    title: 'Book Your Stay',
-    image: 'https://picsum.photos/id/1010/500/300',
-    description: 'Book your perfect stay at our luxurious properties.',
-    price: '6500',
-  }
-]
+
 
 export default function Home() {
+  const [cardData, setCardData] = useState([]);
+  
+  const  fetchData= async()=>{
+    const response =await fetch('https://cdn.jsdelivr.net/gh/abbassiddiqi/airbnb-api@bbd1300a/flats.json');
+    const data = await response.json();
+    setCardData(data);
+  } 
+  
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  
   return (
     <div >
         <IconsList/>
-        <div className="flex flex-wrap p-12 gap-7">
+        <div className="flex flex-wrap justify-center p-12 gap-7 ">
           {
             cardData.map((card, index) => 
               <Card key={index} card={card} />
             )
           }
+          <Card  card={{}} />
         </div>
         
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,12 +3,12 @@ import Modal from '../Modal'
 import { Link } from 'react-router-dom';
 
 export default function Card({card}) {
-    const {title, description, image, price, live} = card;
+    const {name, description, imageUrl, price, live, priceCurrency} = card;
     return (
         <>
         <Link to='/about' className="w-[300px] block overflow-hidden">
         <div className='h-[300px] w-[300px] rounded-xl mb-4 bg-airstay-red relative overflow-hidden'>
-            <img className='absolute object-cover w-full h-full' src={image} alt={title} />
+            <img className='absolute object-cover w-full h-full' src={imageUrl} alt={name} />
             <div className='flex p-4'>
                 { live && <button className='px-4 rounded-full py-[2px] bg-shadow/80 z-20'> Live </button>}
                 <button className='h-[28px] ms-auto flex items-center justify-center w-[28px] rounded-full bg-shadow/80 hover:bg-neutral-50 hover:scale-[1.1] transition-all z-20' onClick={() => document.getElementById('my_modal_2').showModal()}>
@@ -16,9 +16,9 @@ export default function Card({card}) {
                 </button>
             </div>
         </div>
-            <h1> {title} </h1>
+            <h1> {name} </h1>
             <p className='text-light'>{description}</p>
-        <p> ₹{price} per person </p>
+        <p> {priceCurrency}{price} per person </p>
         </Link>
         <Modal classNames="h-[400px] max-w-[600px]" modal_id="my_modal_2">
                     <h3 className="text-5xl font-bold">Hello!</h3>
